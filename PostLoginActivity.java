0ae16307a7db98934f5422e94da4ade5294aa6ec
Jack Grundy <JackMGrundy@gmail.com>
2016-07-10 19:29:40 +0100

    Add files via upload
    Login

---
+++ PostLoginActivity.java
@@ -0,0 +1,16 @@
+package com.jgrundy.logintask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by JGrundy on 7/9/16.
+ */
+public class PostLoginActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_login_activity);
+    }
+}
